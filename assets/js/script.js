671c04e1a5288a6f393b1255877983a11914b837
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-02-06 17:27:47 +0600

    slider dots design completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,6 +95,29 @@
     } 
   ]
   });
+  $('.services-slider-two').slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+   
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      },
+    } 
+  ]
+  });
   
   // testimonial slider js
   $('.testimonial-slider').slick({
